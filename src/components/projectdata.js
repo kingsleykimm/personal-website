35b4087c7eec793a69ca25df94c480e4c5069cfc
Kingsley Kim <kingsleykimm@gmail.com>
2023-07-05 16:16:04 -0400

    Edited projectdata.js

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -1,6 +1,13 @@
 
 const projectdata = [
     {
+        title: "Time Series",
+        image: "https://github.com/UVA-MLSys/Interpretable-Timeseries/blob/main/docs/images/coronavirus.jpg?raw=true",
+        desc: "Website we built for the time series research project we did in my internship",
+        gitLink: "https://github.com/UVA-MLSys/Interpretable-Timeseries",
+        siteLink: "https://uva-mlsys.github.io/Interpretable-Timeseries/pages/introduction.html"
+    },
+    {
         title: "Hoober",
         image: "https://i.ibb.co/312ywyK/Screenshot-2023-06-04-at-15-27-41-React-App.png",
         desc: "Web app built for UVA students looking to carpool to UVA events or to their hometowns",
